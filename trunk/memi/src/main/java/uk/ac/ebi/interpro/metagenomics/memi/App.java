8b4f0f8e7bc4c659c83611379dd47f4ff75a4c90
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-03 12:37:00 +0000

    first commit of the Memi project

---
+++ trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/App.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.interpro.metagenomics.memi;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
