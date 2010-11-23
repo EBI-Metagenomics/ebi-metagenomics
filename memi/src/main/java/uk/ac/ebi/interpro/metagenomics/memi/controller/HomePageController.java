25d2f88985fdf4c7f494c14070a9a9417a17efcd
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-23 17:34:15 +0000

    New. Controller for the MG portal home page.

---
+++ memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/HomePageController.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.interpro.metagenomics.memi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.JstlView;
+import uk.ac.ebi.interpro.metagenomics.memi.dao.StudyDAO;
+import uk.ac.ebi.interpro.metagenomics.memi.model.Study;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Represents the controller for the home page.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+@Controller
+@RequestMapping("/homePage")
+public class HomePageController {
+
+    private StudyDAO studyDAO;
+
+    @Autowired
+    public void setStudyDAO(StudyDAO studyDAO) {
+        this.studyDAO = studyDAO;
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView createModel() {
+        List<Study> studies = studyDAO.getStudiesByVisibility(true);
+        Map model = new HashMap();
+        model.put("studies", studies);
+//        JstlView view = new JstlView();
+//        view.setUrl("/WEB-INF/jsp/widget_list.jsp");
+        return new ModelAndView("homePage", model);
+    }
+}
