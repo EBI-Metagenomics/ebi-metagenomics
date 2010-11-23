4c6d58187a88984d27883c39865abd95fb5be8a8
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-23 17:50:25 +0000

    New. Login form object.

---
+++ memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/forms/LoginForm.java
@@ -0,0 +1,43 @@
+package uk.ac.ebi.interpro.metagenomics.memi.forms;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
+/**
+ * Represents a customized login form.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public class LoginForm {
+
+    @NotEmpty
+    @Size(min = 1, max = 30)
+//    @Email
+//    TODO: Reactivate email annotation (deactivate because the test user has not a valid email address)
+    private String emailAddress;
+
+    @NotEmpty
+    @Size(min = 1, max = 12)
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
