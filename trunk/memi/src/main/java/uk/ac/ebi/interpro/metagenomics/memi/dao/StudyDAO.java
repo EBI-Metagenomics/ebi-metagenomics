0de44fda797d5d139f61f4c4874cb5845d7d86b6
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-23 17:48:53 +0000

    New. Data access objects for studies.

---
+++ trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/dao/StudyDAO.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.interpro.metagenomics.memi.dao;
+
+import uk.ac.ebi.interpro.metagenomics.memi.model.Study;
+
+import java.util.List;
+
+/**
+ * Represents the data access object interface for studies.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public interface StudyDAO {
+
+    /* Returns all studies which are available from database */
+    public List<Study> getStudies();
+
+    /* If TRUE specified, then it will return all public available studies, OTHERWISE it will return only private studies */
+    public List<Study> getStudiesByVisibility(boolean isPublic);
+
+    /* self-explanatory */
+    public Study getStudyById(long id);
+
+    /* self-explanatory */
+    public void deleteStudy(Study study);
+}
