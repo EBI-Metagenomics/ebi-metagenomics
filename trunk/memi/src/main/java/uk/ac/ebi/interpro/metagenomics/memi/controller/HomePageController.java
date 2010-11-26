1b014cf1681cdf44d75169a0b6236c90e3bcce9e
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-26 10:27:17 +0000

    Refactored the controller to make it more simple. Added new @ModelAttribute annotated method to provide dao objects.

--- trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/HomePageController.java
+++ trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/HomePageController.java
@@ -1,42 +1,56 @@
 package uk.ac.ebi.interpro.metagenomics.memi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import uk.ac.ebi.interpro.metagenomics.memi.dao.NewsDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.StudyDAO;
+import uk.ac.ebi.interpro.metagenomics.memi.model.News;
 import uk.ac.ebi.interpro.metagenomics.memi.model.Study;
 
-import java.util.HashMap;
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
- * Represents the controller for the home page.
+ * Represents the controller for the MG portal home page.
  *
  * @author Maxim Scheremetjew, EMBL-EBI, InterPro
  * @version $Id$
  * @since 1.0-SNAPSHOT
  */
 @Controller
-@RequestMapping("/homePage")
+@RequestMapping(value = "/homePage")
 public class HomePageController {
 
+    @Resource
     private StudyDAO studyDAO;
 
-    @Autowired
-    public void setStudyDAO(StudyDAO studyDAO) {
-        this.studyDAO = studyDAO;
-    }
+    @Resource
+    private NewsDAO newsDAO;
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView createModel() {
+    public String initPage() {
+        return "homePage";
+    }
+
+    @ModelAttribute("studies")
+    public List<Study> populateStudyList() {
         List<Study> studies = studyDAO.getStudiesByVisibility(true);
-        ModelMap model = new ModelMap();
-        model.put("studies", studies);
-        return new ModelAndView("homePage", model);
+        if (studies == null) {
+            studies = new ArrayList<Study>();
+        }
+        return studies;
+    }
+
+    @ModelAttribute("newsList")
+    public List<News> populateNewsList() {
+        List<News> newsList = newsDAO.getLatestNews();
+        if (newsList == null) {
+            newsList = new ArrayList<News>();
+        }
+        return newsList;
     }
 }
