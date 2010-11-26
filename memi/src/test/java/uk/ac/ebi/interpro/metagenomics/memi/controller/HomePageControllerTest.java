be954735b05e762f327b68d1c783ccf42f2b1944
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-11-26 11:06:04 +0000

    Refactored unit tests and added a new test for the news list model attribute.

--- memi/src/test/java/uk/ac/ebi/interpro/metagenomics/memi/controller/HomePageControllerTest.java
+++ memi/src/test/java/uk/ac/ebi/interpro/metagenomics/memi/controller/HomePageControllerTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.ModelAndViewAssert;
-import org.springframework.web.servlet.ModelAndView;
+import uk.ac.ebi.interpro.metagenomics.memi.dao.NewsDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.StudyDAO;
+import uk.ac.ebi.interpro.metagenomics.memi.model.News;
 import uk.ac.ebi.interpro.metagenomics.memi.model.Study;
 
 import java.lang.reflect.Field;
@@ -32,33 +32,49 @@ public class HomePageControllerTest extends TestCase {
     public void setUp() throws Exception {
         this.controller = new HomePageController();
 
-        Field daoField = HomePageController.class.
+        //Replace study DAO for simpler testing
+        Field studyDaoField = HomePageController.class.
                 getDeclaredField("studyDAO");
-        daoField.setAccessible(true);
+        studyDaoField.setAccessible(true);
         StudyDAO newStudyDAO = new StudyDAOTestImpl();
-        daoField.set(controller, newStudyDAO);
+        studyDaoField.set(controller, newStudyDAO);
+        //Replace news DAO
+        Field newsDaoField = HomePageController.class.
+                getDeclaredField("newsDAO");
+        newsDaoField.setAccessible(true);
+        NewsDAO newNewsDAO = new NewsDAOTestImpl();
+        newsDaoField.set(controller, newNewsDAO);
+
+
     }
 
     @Test
-    public void testCreateModel() throws Exception {
-        ModelAndView mav = controller.createModel();
-        assertTrue(mav.getModel() != null);
-        assertEquals(1, mav.getModel().size());
-//          tests the requested ModelAndView object
-        ModelAndViewAssert.assertViewName(mav, "homePage");
-        ModelAndViewAssert.assertModelAttributeAvailable(mav, "studies");
-        ModelAndViewAssert.assertAndReturnModelAttributeOfType(mav, "studies", List.class);
-//          creates an expected MAV and checks against the actual
-//        TODO: Check why assertModelAttributeValues does not use the implemented equals method of the Study object
-//        StudyDAO studyDAO = new StudyDAOTestImpl();
-//        List<Study> studies = studyDAO.getStudiesByVisibility(true);
-//        ModelMap expectedModel = new ModelMap();
-//        expectedModel.put("studies", studies);
-//        ModelAndViewAssert.assertModelAttributeValues(mav, expectedModel);
+    public void testInitPage() throws Exception {
+        assertEquals("homePage", controller.initPage());
+    }
+
+    @Test
+    public void testPopulateStudyList() throws Exception {
+        List<Study> studies = controller.populateStudyList();
+        assertNotNull(studies);
+        assertEquals(10, studies.size());
+        assertEquals("study_1", studies.get(0).getStudyName());
+        assertEquals(true, studies.get(0).isPublic());
+        assertEquals("study_10", studies.get(9).getStudyName());
+        assertEquals(true, studies.get(9).isPublic());
+    }
+
+    @Test
+    public void testPopulateNewsList() throws Exception {
+        List<News> newsList = controller.populateNewsList();
+        assertNotNull(newsList);
+        assertEquals(10, newsList.size());
+        assertEquals("Headline 1", newsList.get(0).getNewsHeadline());
+        assertEquals("message", newsList.get(0).getNewsMsg());
     }
 
     /**
-     * This DAO implementation should only used for JUnit test.
+     * This DAO implementation should only be used for JUnit test.
      */
     class StudyDAOTestImpl implements StudyDAO {
 
@@ -109,6 +125,31 @@ public class HomePageControllerTest extends TestCase {
         @Override
         public void deleteStudy(Study study) {
             //To change body of implemented methods use File | Settings | File Templates.
+        }
+    }
+
+    /**
+     * This DAO implementation should only be used for JUnit test.
+     */
+    class NewsDAOTestImpl implements NewsDAO {
+
+        public NewsDAOTestImpl() {
+        }
+
+        @Override
+        public List<News> getLatestNews() {
+            List<News> result = new ArrayList<News>();
+            result.add(new News("Headline 1", "message"));
+            result.add(new News("Headline 2", "message"));
+            result.add(new News("Headline 3", "message"));
+            result.add(new News("Headline 4", "message"));
+            result.add(new News("Headline 5", "message"));
+            result.add(new News("Headline 6", "message"));
+            result.add(new News("Headline 7", "message"));
+            result.add(new News("Headline 8", "message"));
+            result.add(new News("Headline 9", "message"));
+            result.add(new News("Headline 10", "message"));
+            return result;
         }
     }
 }
