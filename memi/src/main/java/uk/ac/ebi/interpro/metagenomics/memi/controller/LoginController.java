95b6e1b3b41eefcd97317d9b9af112d61e69ab9e
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-12-01 16:44:08 +0000

    Added a logger and simple check if the database connection is still alive.

--- memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/LoginController.java
+++ memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/LoginController.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.interpro.metagenomics.memi.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -31,6 +33,8 @@ public class LoginController {
     @Resource(name = "submitterDAO")
     private SubmitterDAO submitterDAO;
 
+    private final Log log = LogFactory.getLog(LoginController.class);
+
     @RequestMapping(method = RequestMethod.GET)
     public String initForm(ModelMap model) {
         LoginForm loginForm = new LoginForm();
@@ -46,6 +50,10 @@ public class LoginController {
         loginForm = (LoginForm) model.get("loginForm");
         if (loginForm != null) {
             String emailAddress = loginForm.getEmailAddress();
+            if (!submitterDAO.isDatabaseAlive()) {
+                result.addError(new FieldError("loginForm", "emailAddress", "Database is down! We are sorry for that."));
+                return "loginForm";
+            }
             Submitter submitter = submitterDAO.getSubmitterByEmailAddress(emailAddress);
             if (submitter != null) {
                 if (!submitter.getPassword().equals(loginForm.getPassword())) {
@@ -53,6 +61,7 @@ public class LoginController {
                     return "loginForm";
                 }
             } else {
+                log.warn("Could not find any submitter for the specified email address: " + emailAddress);
                 result.addError(new FieldError("loginForm", "emailAddress", "Incorrect login data!"));
                 return "loginForm";
             }
