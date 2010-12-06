107101d6e68dae74b3e17aa56dbeb4396991d5f9
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-12-06 16:04:35 +0000

    Changed url pattern to /memi/*

--- trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/StudyOverviewController.java
+++ trunk/memi/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/StudyOverviewController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.SampleDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.StudyDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.model.Sample;
