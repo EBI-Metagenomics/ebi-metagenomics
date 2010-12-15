c37d41aef65533c2100571b4dcf691e0e21d8e94
Maxim.Scheremetjew <Maxim.Scheremetjew@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2010-12-15 16:29:35 +0000

    Intermediate state after Hibernate integration and modularization.

---
+++ memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/dao/EmgSampleDAOImpl.java
@@ -0,0 +1,88 @@
+package uk.ac.ebi.interpro.metagenomics.memi.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.interpro.metagenomics.memi.model.EmgSample;
+import uk.ac.ebi.interpro.metagenomics.memi.model.EmgStudy;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Represents the implementation class of {@link uk.ac.ebi.interpro.metagenomics.memi.dao.EmgSampleDAO}
+ * TODO: Associate with Hibernate (all methods still return mock-up objects)
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+//@Repository
+@Transactional
+public class EmgSampleDAOImpl implements EmgSampleDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public EmgSampleDAOImpl() {
+    }
+
+    @Override
+    public EmgSample insert(EmgSample newInstance) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Collection<EmgSample> insert(Collection<EmgSample> newInstances) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void update(EmgSample modifiedInstance) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public EmgSample read(String id) {
+        Session session = sessionFactory.getCurrentSession();
+        if (session != null) {
+            return (EmgSample) session.get(EmgSample.class, id);
+        }
+        return null;
+    }
+
+    @Override
+    public EmgSample readDeep(String id, String... deepFields) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void delete(EmgSample persistentObject) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Long count() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<EmgSample> retrieveAll() {
+        Session session = sessionFactory.getCurrentSession();
+        if (session != null) {
+            return session.createCriteria(EmgSample.class).list();
+        }
+        return null;
+    }
+
+    @Override
+    public int deleteAll() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Long getMaximumPrimaryKey() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/dao/EmgStudyDAO.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.interpro.metagenomics.memi.dao;
+
+import uk.ac.ebi.interpro.metagenomics.memi.model.EmgStudy;
+import uk.ac.ebi.interpro.scan.genericjpadao.GenericDAO;
+
+/**
+ * Represents the data access object interface for studies.
+ *
+ * @author Maxim Scheremetjew, EMBL-EBI, InterPro
+ * @version $Id$
+ * @since 1.0-SNAPSHOT
+ */
+public interface EmgStudyDAO extends GenericDAO<EmgStudy, String> {
+
+}
