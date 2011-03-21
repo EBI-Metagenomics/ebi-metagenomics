a8b9ed5a40c1137fb7b86a233e9ff855080320e3
mr.matthew.fraser <mr.matthew.fraser@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2011-03-21 12:09:01 +0000

    RSS feed display enhancements - get around some limitations of using Twitter

--- trunk/memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/feed/CachingRomeClient.java
+++ trunk/memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/feed/CachingRomeClient.java
@@ -3,17 +3,19 @@ package uk.ac.ebi.interpro.metagenomics.memi.feed;
 import com.googlecode.ehcache.annotations.Cacheable;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
 import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
 
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.xml.sax.InputSource;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This implementation caches entries.
@@ -61,7 +63,10 @@ public final class CachingRomeClient implements RomeClient {
         int count = 0;
         for (Object o : syndFeed.getEntries()) {
             if (count < limit) {
-                entries.add((SyndEntry) o);                 
+                String title = ((SyndEntry) o).getTitle();
+                title = transformTwitterText(title);
+                ((SyndEntry)o).setTitle(title);
+                entries.add((SyndEntry) o);
             }
             else {
                 break;
@@ -70,6 +75,39 @@ public final class CachingRomeClient implements RomeClient {
         }
         // TODO: Create our own model for news, including entries and URL
         return entries;
-    }  
-    
+    }
+
+    private final static String transformTwitterText(String text) {
+        // Strip username off the front of the message
+        String twitterPrefix = "EBImetagenomics: ";
+        if (text.startsWith(twitterPrefix)) {
+            text = text.substring(twitterPrefix.length());
+        }
+
+        // Find links contained within RSS feed text
+        String regex = "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        Set<String> urlSet = new HashSet<String>();
+        while (matcher.find()) {
+            urlSet.add(matcher.group());
+        }
+
+        // Now make the contained links linkable
+        int maxUrlTextLength = 35;
+        for (String url : urlSet) {
+            String urlText;
+            if (url.length() > maxUrlTextLength) {
+                urlText = url.substring(0, maxUrlTextLength - 1) + "...";
+            }
+            else {
+                urlText = url;
+            }
+            String replacedUrl = "<a href=\"" + url + "\">" + urlText + "</a>";
+            text = text.replaceAll(url, replacedUrl);
+        }
+
+        return text;
+    }
+
 }
