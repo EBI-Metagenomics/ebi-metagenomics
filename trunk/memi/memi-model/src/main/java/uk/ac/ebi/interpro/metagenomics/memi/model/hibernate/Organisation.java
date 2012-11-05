4c676d7d702353640516916ffd4ff86b983e692f
Maxim.Scheremetjew@gmail.com <Maxim.Scheremetjew@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2012-11-05 09:40:24 +0000

    First version of the hibernate model for the new login.

---
+++ trunk/memi/memi-model/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/model/hibernate/Organisation.java
@@ -0,0 +1,47 @@
+package uk.ac.ebi.interpro.metagenomics.memi.model.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORGANISATION")
+public class Organisation {
+    @Id
+    @Column(name = "ORGANISATION_ID")
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "STUDY_SEQ")
+//    @SequenceGenerator(
+//            name = "STUDY_SEQ",
+//            sequenceName = "STUDY_SEQ")
+////            allocationSize = 1
+    private long id;
+
+    @Column(name = "ORGANISATION_NAME", nullable = false, length = 30)
+    private String organisationName;
+
+    @Column(name = "ORGANISATION_TYPE", length = 30)
+    @Enumerated(EnumType.STRING)
+    private OrganisationType type;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOrganisationName() {
+        return organisationName;
+    }
+
+    public void setOrganisationName(String organisationName) {
+        this.organisationName = organisationName;
+    }
+
+    public OrganisationType getType() {
+        return type;
+    }
+
+    public void setType(OrganisationType type) {
+        this.type = type;
+    }
+}
