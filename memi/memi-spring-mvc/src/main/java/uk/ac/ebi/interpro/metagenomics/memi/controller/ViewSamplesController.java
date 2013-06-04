33236061e94e0604dd219c3b23765ff363e7776b
sebpca <sebpca@gmail.com>
2013-06-04 16:11:44 +0000

    Change title column to analysis results to make the result text more clear

--- memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/ViewSamplesController.java
+++ memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/ViewSamplesController.java
@@ -197,7 +197,7 @@ public class ViewSamplesController extends AbstractController implements IContro
         result.add("Sample name");
         result.add("Project name");
         result.add("Source");
-        result.add("Analysis");
+        result.add("Analysis results");
         return result;
     }
 
