279ae77f549dc836c4cdc0170a00e90cf8468f49
francois.bucchini@gmail.com <francois.bucchini@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2014-06-13 11:24:50 +0000

    Newer R scripts

---
+++ branches/comparison_tool/memi-web/R/launch_v8.R
@@ -0,0 +1,161 @@
+# Launch script. Will create the abundance table and then generate each visualization
+# All is saved in HTML files which are parsed and then displayed.
+# Nb: Logic changed for parameters. Older versions won't work anymore.
+
+# Console log
+message(paste(Sys.time(),'[R - Message] Launched general R script launch.R'))
+
+# Loading of needed libraries and sources
+library(methods)
+library(rCharts)
+
+# A boolean to disable heatmap generation as it doesn't work on the server ...
+commit = TRUE
+
+# Allow R to read command arguments
+parameters <- commandArgs(TRUE)
+
+# Load pre-processing script
+source("R/scripts/PreProcess_v11.R")
+
+# EMG website colors
+EMGcolors <- c('#058DC7','#50B432','#ED561B','#EDEF00','#24CBE5','#64E572','#FF9655','#FFF263','#6AF9C4','#DABE88')
+
+# A small operator that is quite useful
+"%w/o%" <- function(x, y) x[!x %in% y] # ->  x without y 
+
+# Create table to retrieve names and type from a GO slim term
+GOslimTable <- read.table("R/ressources/GOslim_names.csv", sep = ",", quote = "\"", 
+                          col.names = c("GO", "function", "type"), colClasses = c("character", "character", "character"))
+
+# Small check to see number of arguments. Uncomment to use.
+# print(length(parameters))
+
+name <- parameters[1] # output file name
+files <- (strsplit(parameters[2],split=',')[[1]]) # Creation of a vector of file names from the list in parameter
+data <- parameters[3] # Chosen data
+sampleNames <- (strsplit(parameters[4],split=',')[[1]]) # Sample names
+
+# Advanced settings
+stack <- as.numeric(parameters[5]) # Stacking threshold for stacked bars
+hmParameters <- (strsplit(parameters[6],split=',')[[1]]) #Heatmap parameters
+
+
+# Print arguments to see if everything is okay. Uncomment to use.
+print(parameters)
+
+# Launch abundance table creation script and print it for debugging.
+abTable = CreateAbTable(name, files, data, sampleNames)
+# print(abTable) # Uncomment line to print
+
+# First try to deal with the full GO stuff ...
+if(data=='GO') {
+  mostVarNames = names(sort(apply(abTable, 2, var),decreasing=TRUE))
+  abTable = abTable [,c(mostVarNames[1:120])]
+}
+
+# Dealing with all visualizations
+
+#########################################
+############### Overview ################
+#########################################
+
+# Call needed methods
+source('R/scripts/PieChartOverview_v4.R')
+# Use methods (generate, grab useful data)
+graphCode = generateOverview(abTable)
+# Write final result file
+write(graphCode,paste('R/tmpGraph/',name,'_overview','.htm',sep=''))
+
+
+#########################################
+############### Barcharts ###############
+#########################################
+
+fullBarCode <- 'You have more than 10 samples, cannot display bar chart visualization.'
+
+if(length(files) <= 10) {
+source('R/scripts/GOslimBar_v3.R')
+bio = CreateGraphForCategory(abTable,'biological_process')
+mol = CreateGraphForCategory(abTable,'molecular_function')
+cell = CreateGraphForCategory(abTable,'cellular_component')
+
+# Save
+bio$save(paste('R/tmpGraph/',name,'_bio.htm',sep=''),cdn=TRUE)
+mol$save(paste('R/tmpGraph/',name,'_mol.htm',sep=''),cdn=TRUE)
+cell$save(paste('R/tmpGraph/',name,'_cell.htm',sep=''),cdn=TRUE)
+
+# Grab some code
+fileNames = c(paste('R/tmpGraph/',name,'_bio.htm',sep=''),
+paste('R/tmpGraph/',name,'_mol.htm',sep=''),
+paste('R/tmpGraph/',name,'_cell.htm',sep=''))
+fileContent = c(paste(readLines(fileNames[1]),collapse='\n'),paste(readLines(fileNames[2]),collapse='\n'),paste(readLines(fileNames[3]),collapse='\n'))
+grabbedStyle <- c() # regmatches(fileContent[1], regexpr('<script src=\'http://.+</style>', fileContent[1]))
+grabbedBio <- regmatches(fileContent[1], regexpr('<div id=\'chart.+</script>', fileContent[1]))
+grabbedMol <- regmatches(fileContent[2], regexpr('<div id=\'chart.+</script>', fileContent[2]))
+grabbedCell <- regmatches(fileContent[3], regexpr('<div id=\'chart.+</script>', fileContent[3]))
+
+# Remove the files to keep space
+file.remove(fileNames)
+
+# Store and write the final result. If too many samples, write an error message instead.
+fullBarCode <- c(grabbedStyle,grabbedMol,grabbedBio,grabbedCell)
+}
+
+write(fullBarCode,paste('R/tmpGraph/',name,'_bar','.htm',sep=''))
+
+
+
+#########################################
+####### GO slim | Stacked Bars ##########
+#########################################
+
+# Call needed methods
+source('R/scripts/GOslimStack_v2.R')
+# Use methods, save, grab useful data
+totalCode = c()
+stackedBars = list(CreateGraph(abTable,stack,'biological_process'),CreateGraph(abTable,stack,'cellular_component'),CreateGraph(abTable,stack,'molecular_function'))
+divHelper = c('<div id=\"stack_bio\">','<div id=\"stack_cell\">','<div id=\"stack_mol\">') # I'll explain this later. Lazy.
+for(i in 1:3) {
+stackedBars[[i]]$save(paste('R/tmpGraph/',name,'.htm',sep=''),cdn=TRUE) # save temporary file
+# Grab some code
+fileName = paste('R/tmpGraph/',name,'.htm',sep='')
+fileContent = paste(readLines(fileName),collapse='\n')
+grabbedStyle <- c()#regmatches(fileContent, regexpr('<script src=\'http://.+</style>', fileContent))
+grabbedGraph <- regmatches(fileContent, regexpr('<div id=\'chart.+</script>',fileContent))
+totalCode = c(totalCode,paste(divHelper[i],grabbedGraph,'</div>'))
+# Remove the files to keep space
+file.remove(fileName)
+}
+# Write final result file
+write(c(totalCode),paste('R/tmpGraph/',name,'_stack','.htm',sep=''))
+
+#########################################
+########### Static Heatmap ##############
+#########################################
+hmCode = '<p><i>Generation of PNG heatmaps is not working on the server ...</i></p>'
+if(!commit) {
+source('R/scripts/StaticHeatmap_v3.R')
+pathAndName = paste('src/main/webapp/img/comparison/',c('bio_','cell_','mol_'),name,'.png',sep='')
+GenerateHeatmap(abTable,'biological_process',pathAndName[1],hmParameters)
+GenerateHeatmap(abTable,'cellular_component',pathAndName[2],hmParameters)
+GenerateHeatmap(abTable,'molecular_function',pathAndName[3],hmParameters)
+
+# A bit tricky with the JSTL paths ... But display this anyway
+hmCode = paste('<div id="hm_bio"><img src=\"','/metagenomics/img/comparison/',paste0('bio_',name),'.png\"></div>','<div id="hm_cell"><img src=\"','/metagenomics/img/comparison/',paste0('cell_',name),'.png\"></div>','<div id="hm_mol"><img src=\"','/metagenomics/img/comparison/',paste0('mol_',name),'.png\"></div>',sep='')
+}
+write(hmCode,paste('R/tmpGraph/',name,'_hm','.htm',sep=''))
+
+#########################################
+################ Table ##################
+#########################################
+source('R/scripts/Table_v2.R')
+coucou = DataTableGen(abTable)
+write(coucou,paste('R/tmpGraph/',name,'_table','.htm',sep=''))
+
+
+# Delete table 
+file.remove(paste('R/tables/',name,'.txt',sep=''))
+
+# Console log
+message(paste(Sys.time(),'[R - Message] R scripts over.'))
---
+++ branches/comparison_tool/memi-web/R/scripts/GOslimStack_v2.R
@@ -0,0 +1,84 @@
+# Create the right charts if user has chosen go slim and stacked barcharts
+
+# Console log
+message(paste(Sys.time(),'[R - Message] Launched R script GOslimStack.R'))
+
+# A small operator that is quite useful
+"%w/o%" <- function(x, y) x[!x %in% y] # ->  x without y 
+
+# Load libraries
+library(data.table)
+
+divideAbTable = function(abTable,category) {
+  abTableCategory <- abTable[,grepl(category,colnames(abTable))]
+  return(abTableCategory)
+}
+
+# Go from divided abundance table to edible data for rChart  
+rChartsFixing = function(abundanceTable,percent=FALSE){
+  nbSamples = nrow(abundanceTable)
+  nbObs = ncol(abundanceTable)
+  maxOfAll = rowSums(abundanceTable)
+  recordGO=c()
+  recordName= c()
+  recordSample=c()
+  recordValue=c()
+  for(i in 1:nbObs){
+    for(j in 1:nbSamples) {
+      recordGO = c(recordGO,strsplit(colnames(abundanceTable)[i],split='\\|')[[1]][1])
+      # GO names in a separate place ? Could be changed in the future
+      recordName <- c(recordName,strsplit(colnames(abundanceTable)[i],split='\\|')[[1]][2])
+      recordSample = c(recordSample,rownames(abundanceTable)[j])
+      if(percent) recordValue=c(recordValue,round(((abundanceTable[j,i]/maxOfAll[i])*100),3))
+      else recordValue=c(recordValue,abundanceTable[j,i])
+    }
+  }
+  
+  newTable=data.table(recordSample,recordGO,recordName,recordValue)
+  setnames(newTable,colnames(newTable),c('sample','GO','GOname','value'))
+  return(newTable)
+}
+
+rChartsStacked = function(abundanceTable,threshold){
+  newTable = abundanceTable
+  maxOfAll = rowSums(abundanceTable)
+  for(i in 1:length(maxOfAll)) {
+    newTable[i,] = round(((newTable[i,]/maxOfAll[i])*100),3)
+  }
+  
+  newTable = rChartsFixing(newTable)
+  # Threshold handling
+  newTable$GO[newTable$value < threshold] = 'Other'
+  newTable$GOname[newTable$value < threshold] = 'Other'
+  newTable = aggregate(value~sample+GO+GOname,data=newTable,FUN=sum)
+  
+  # Bug with rCharts : Need to recreate the missing values (values that were under threshold in some sample(s) and not in others)
+  GO = unique(newTable$GO)
+  samples = unique(newTable$sample)
+  for(j in GO){
+    subTable = newTable[newTable$GO==j,]
+    if((dim(subTable)[1])<length(samples)){
+      addData = data.frame(samples%w/o%subTable$sample,j,subTable$GOname[1],0)
+      colnames(addData)=c('sample','GO','GOname','value')
+      newTable = rbind(newTable,addData)
+    }
+  }
+  newTable = as.data.table(newTable)
+  return(newTable)
+}
+
+# Final function. Yay. For one category, from general table. Cool.
+CreateGraph = function(abundanceTable,threshold,category) {
+  dataChart = rChartsStacked(divideAbTable(abundanceTable,category),threshold)
+  chartWidth = 900+length(unique(dataChart$sample))*50
+  chart <- hPlot(value~sample,data = dataChart[dataChart$GOname!='Other'], group='GOname',type = 'column',group.na = 'NA\'s',title = 'Most frequent GO slim by sample', subtitle = category)
+  chart$chart(width = chartWidth, height = 700)
+  chart$series(data = dataChart$value[dataChart$GOname=='Other'], name = paste('other (less than ',as.character(threshold),'%)',sep=''),type='column', color = '#B9B9B9')
+  chart$plotOptions(column = list(stacking = "percent")) 
+  chart$xAxis(categories=c(unique(dataChart$sample)))
+  chart$yAxis(title=list(text='Relative abundance (%)'))
+  chart$legend(layout='vertical',align='right',verticalAlign='top',x=-10,y=100)
+  chart$colors(EMGcolors)
+  chart$exporting(enabled = T)
+  return(chart)
+}
