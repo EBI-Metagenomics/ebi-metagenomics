a44b6bb46cb9d2c726fc4287ff788d27b17ad967
maxim.scheremetjew@gmail.com <maxim.scheremetjew@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2014-09-05 14:27:53 +0000

    Rolled back execution command.

--- branches/comparison_tool/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/CompareController.java
+++ branches/comparison_tool/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/CompareController.java
@@ -163,8 +163,8 @@ public class CompareController extends AbstractController implements IController
         // Execution command to launch the comparison job
         final char WHITESPACE = ' ';
         String executionCommand;
-        executionCommand = "\"" + rInstallationLocation + "\"" + WHITESPACE + rScriptPath.replace("\\","\\") + "" + rScriptName + WHITESPACE + rTmpFileDirectory.replace("\\","\\") + WHITESPACE +
-                rScriptPath.replace("\\","\\") + WHITESPACE +  rImgDirectory.replace("\\","\\") + WHITESPACE + uniqueOutputName + WHITESPACE + rFriendlyFileList.replace("\\","\\") + WHITESPACE + comparisonForm.getUsedData() +
+        executionCommand = rInstallationLocation + WHITESPACE + rScriptPath + "/" + rScriptName + WHITESPACE + rTmpFileDirectory + WHITESPACE +
+                rScriptPath + WHITESPACE + rImgDirectory + WHITESPACE + uniqueOutputName + WHITESPACE + rFriendlyFileList + WHITESPACE + comparisonForm.getUsedData() +
                 WHITESPACE + rFriendlySampleNames + WHITESPACE + comparisonForm.getStackThreshold() + WHITESPACE + hmPar + WHITESPACE + comparisonForm.getGOnumber();
 //            executionCommand = "Rscript R/simple.R";
         // Print the command we will use to see if it's correct (format / order of parameters)
