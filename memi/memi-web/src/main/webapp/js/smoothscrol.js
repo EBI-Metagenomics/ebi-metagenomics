9ec7c2c2bca3c2efef28e9c91b44a6671572ff85
sebpca <sebpca@gmail.com>
2015-01-29 15:39:15 +0000

    Solve bug scrolling for comparison tool tabs

--- memi/memi-web/src/main/webapp/js/smoothscrol.js
+++ memi/memi-web/src/main/webapp/js/smoothscrol.js
@@ -1,6 +1,6 @@
-
+//remove tab ids used in sample comparison tool for smooth effect
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').not('#ui-id-1, #ui-id-2, #ui-id-3, #ui-id-4, #ui-id-5').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
