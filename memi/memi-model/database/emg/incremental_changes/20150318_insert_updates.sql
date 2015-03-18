a2796d724759be65c06c4fe41171b3a92bbdaf59
Maxim <maxim@ebi.ac.uk>
2015-03-18 17:14:40 +0000

    Simple data update.

---
+++ memi/memi-model/database/emg/incremental_changes/20150318_insert_updates.sql
@@ -0,0 +1,11 @@
+--Added additional analysis status
+--Updated pipeline tool versions numbers and descriptions
+
+--Author: Maxim Scheremetjew, EMBL-EBI, InterPro
+
+INSERT INTO analysis_status (analysis_status_id,analysis_status) values (5,'suppressed');
+
+UPDATE pipeline_tool SET version='3.2.2' WHERE version='open-3.2.2';
+UPDATE pipeline_tool SET version='5.0-beta' WHERE version='5.0 (beta release)';
+UPDATE pipeline_tool SET description='An application for finding fragmented genes in short reads.' WHERE tool_name='FragGeneScan';
+UPDATE pipeline_tool SET description='A set of freely available tools for biological computation written in Python.' WHERE tool_name='Biopython';
