a40462741a2f359e7e4020a811f07903b8bf385d
maxim.scheremetjew@gmail.com <maxim.scheremetjew@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2015-03-18 17:33:14 +0000

    Added another updated statement.

--- trunk/memi/memi-model/database/emg/incremental_changes/20150318_insert_updates.sql
+++ trunk/memi/memi-model/database/emg/incremental_changes/20150318_insert_updates.sql
@@ -9,3 +9,4 @@ UPDATE pipeline_tool SET version='3.2.2' WHERE version='open-3.2.2';
 UPDATE pipeline_tool SET version='5.0-beta' WHERE version='5.0 (beta release)';
 UPDATE pipeline_tool SET description='An application for finding fragmented genes in short reads.' WHERE tool_name='FragGeneScan';
 UPDATE pipeline_tool SET description='A set of freely available tools for biological computation written in Python.' WHERE tool_name='Biopython';
+UPDATE pipeline_tool SET description='A computer program for selecting ribosomal RNA encoding sequences from metagenomic and metatranscriptomic shotgun libraries.' WHERE tool_name='rRNASelector';
