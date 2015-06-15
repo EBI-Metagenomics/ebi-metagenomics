439f8ccfd059048959f5e8aa580eeade14f6d175
maxim.scheremetjew@gmail.com <maxim.scheremetjew@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2015-06-15 14:32:41 +0000

    Removed obsolete branch from project.

--- branches/authentication/memi/memi-spring-mvc/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/springmvc/model/analysisPage/TaxonomicAnalysisFileDefinition.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ebi.interpro.metagenomics.memi.springmvc.model.analysisPage;
-
-/**
- * @author Maxim Scheremetjew
- */
-public class TaxonomicAnalysisFileDefinition extends DownloadableFileDefinition {
-}
