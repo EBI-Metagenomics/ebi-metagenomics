faf3a0bd161808f5a2d412f46ace26347adc703b
sebpca@gmail.com <sebpca@gmail.com@79672a62-94ce-731c-b6e4-b7a71cf3b65d>
2015-07-16 14:47:19 +0000

    update order biome column

--- branches/biome_search/memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/ViewStudiesController.java
+++ branches/biome_search/memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/ViewStudiesController.java
@@ -263,8 +263,8 @@ public class ViewStudiesController extends AbstractController implements IContro
      */
     private List<String> getTableHeaderNames() {
         List<String> result = new ArrayList<String>();
-        result.add("Project name");
         result.add("Biome");
+        result.add("Project name");
         result.add("Samples");
         result.add("Last updated");
         return result;
