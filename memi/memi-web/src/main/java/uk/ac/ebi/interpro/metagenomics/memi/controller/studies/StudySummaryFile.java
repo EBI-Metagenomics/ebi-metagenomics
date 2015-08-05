c13569204fafa4a5f0540ad16a34e30f49d710f3
sebpca <sebpca@gmail.com>
2015-08-05 15:17:49 +0000

    Update style and labelling for the download links (project) to make it consistent with the run download

--- memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/studies/StudySummaryFile.java
+++ memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/studies/StudySummaryFile.java
@@ -4,17 +4,17 @@ package uk.ac.ebi.interpro.metagenomics.memi.controller.studies;
  * Expected file names that help to summary a studies details.
  */
 public enum StudySummaryFile {
-    BP_GO_SLIM("BP_GO-slim_abundances", "Abundance tables for the Biological Process GO-slim terms found in the samples from a study.", 1),
-    BP_GO("BP_GO_abundances", "Abundance tables for the Biological Process GO terms found in the samples from a study", 2),
-    CC_GO_SLIM("CC_GO-slim_abundances", "Abundance tables for the Cellular Component GO-slim terms found in the samples from a study.", 3),
-    CC_GO("CC_GO_abundances", "Abundance tables for the Cellular Component GO terms found in the samples from a study.", 4),
-    GO_SLIM("GO-slim_abundances", "Abundance tables for all GO-slim terms found in the samples from a study.", 5),
-    GO("GO_abundances", "Abundance tables for all GO terms found in the samples from a study.", 6),
-    IPR("IPR_abundances", "Abundance tables for the InterPro entries found in the samples from a study.", 7),
-    MF_GO_SLIM("MF_GO-slim_abundances", "Abundance tables for the Molecular Function GO-slim terms found in the samples from a study.", 8),
-    MF_GO("MF_GO_abundances", "Abundance tables for the Molecular Function GO terms found in the samples from a study.", 9),
-    PHYLUM_TAXONOMY("phylum_taxonomy_abundances", "Abundance tables for the phylum_level taxonomies found in the samples from a study.", 10),
-    TAXONOMY("taxonomy_abundances", "Abundance tables for the full taxonomies found in the samples from a study.", 11);
+//    BP_GO_SLIM("BP_GO-slim_abundances", "Biological Process GO-slim terms found in the samples from a study.", 1),
+//    BP_GO("BP_GO_abundances", "Biological Process GO terms found in the samples from a study", 2),
+//    CC_GO_SLIM("CC_GO-slim_abundances", "Cellular Component GO-slim terms found in the samples from a study.", 3),
+//    CC_GO("CC_GO_abundances", "Cellular Component GO terms found in the samples from a study.", 4),
+//    MF_GO_SLIM("MF_GO-slim_abundances", "Molecular Function GO-slim terms found in the samples from a study.", 8),
+//    MF_GO("MF_GO_abundances", "Molecular Function GO terms found in the samples from a study.", 9),
+    GO_SLIM("GO-slim_abundances", "GO slim annotation (TSV)", 7),
+    GO("GO_abundances", "Complete GO annotation (TSV)", 6),
+    IPR("IPR_abundances", "InterPro matches (TSV)", 5),
+//    PHYLUM_TAXONOMY("phylum_taxonomy_abundances", "phylum_level taxonomies found in the samples from a study.", 10),
+    TAXONOMY("taxonomy_abundances", "OTUs and taxonomic assignments (TSV)", 11);
 
     private String filename;
     private String description;
