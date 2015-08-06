fd53e8c0a4bb1a38d9957d056dd0a4d762fc39c1
Mr.Matthew.Fraser <Mr.Matthew.Fraser@gmail.com>
2015-08-06 10:31:45 +0000

    IBU-3866: Bug fix for windows machine

--- memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/services/FileExistenceChecker.java
+++ memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/services/FileExistenceChecker.java
@@ -5,11 +5,15 @@ import java.io.File;
 public final class FileExistenceChecker {
 
     /**
+     * Check if a file or directory exists (and is not hidden).
      * Also checks if file is empty (this may not work for some files;depends on the encoding).
      */
     public static boolean checkFileExistence(final File file) {
-        if (file != null && file.exists() && file.length() != 0) {
-            return true;
+        if (file != null && file.exists() && !file.isHidden()) {
+            if (file.isFile() && file.length() == 0) {
+                return false;
+            }
+            return true; // Directories on Windows machines have length zero
         }
         return false;
     }
