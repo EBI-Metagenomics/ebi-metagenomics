15a268bb87786f22b4d346475f811e305c760ef1
maxim <maxim@ebi.ac.uk>
2015-10-27 15:26:20 +0000

    Python script which allows project bulk downloads of specific file types.

---
+++ memi/memi-web/developers/python/mgportal_bulk_download.py
@@ -0,0 +1,58 @@
+import argparse
+import csv
+import os
+import urllib
+from urllib2 import URLError
+
+if __name__ == "__main__":
+    #    Parse script parameters
+    parser = argparse.ArgumentParser(description="MGPortal bulk download tool.")
+    parser.add_argument("-i", "--input_file",
+        help="Tab-separated file, which contains the mapping between project, sample and run.",
+        required=True)
+    parser.add_argument("-o", "--output_path", help="Location where the download files are stored.",
+        required=True)
+    parser.add_argument("-v", "--version", help="Version of the pipeline used to generate the results.",
+        required=True)
+    parser.add_argument("-t", "--file_type",
+        help="Possible file types are: InterProScan, ProcessedReads, ReadsWithPredictedCDS, ReadsWithMatches, "\
+             "ReadsWithoutMatches, PredictedCDS, PredicatedCDSWithoutAnnotation, PredictedORFWithoutAnnotation"
+        ,
+        required=True)
+    args = vars(parser.parse_args())
+
+    input_file = args['input_file']
+    file_type = args['file_type']
+    output_path = args['output_path'] + "/" + file_type
+    version = args['version']
+    domain = None
+    fileExtension = None
+    if file_type == 'InterProScan':
+        domain = "function"
+        fileExtension = ".tsv.gz"
+    elif file_type == 'PredictedCDS' or file_type == 'PredicatedCDSWithoutAnnotation':
+        domain = "sequences"
+        fileExtension = ".faa.gz"
+    else:
+        domain = "sequences"
+        fileExtension = ".fasta.gz"
+
+    with open(input_file, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for study_id, sample_id, run_id in reader:
+            print study_id + ", " + sample_id + ", " + run_id
+
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+
+            output_file_name = output_path + "/" + run_id + "_" + file_type + fileExtension
+            rootUrl = "https://wwwdev.ebi.ac.uk/metagenomics/projects/%s/samples/%s/runs/%s/results/versions/%s/%s/%s/chunks/1" % (
+                study_id, sample_id, run_id, version, domain, file_type)
+
+            print "Starting the download of the following file..."
+            print rootUrl
+
+            try: urllib.urlretrieve(rootUrl, output_file_name)
+            except URLError as e:
+                print e.reason
+            print "Download finished."
