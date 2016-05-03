87fb9483fe8119e73bdd6aa0fec07a1f971c6e43
CodeGit <maq@ebi.ac.uk>
2016-05-03 08:48:07 +0100

    Implemented run output for individual Project pages

--- memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/studies/OverviewStudyViewController.java
+++ memi/memi-web/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/controller/studies/OverviewStudyViewController.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +14,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.ebi.interpro.metagenomics.memi.controller.MGPortalURLCollection;
 import uk.ac.ebi.interpro.metagenomics.memi.controller.ModelProcessingStrategy;
+import uk.ac.ebi.interpro.metagenomics.memi.core.VelocityTemplateWriter;
 import uk.ac.ebi.interpro.metagenomics.memi.core.tools.MemiTools;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.RunDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.erapro.SubmissionContactDAO;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.extensions.QueryRunsForProjectResult;
 import uk.ac.ebi.interpro.metagenomics.memi.dao.hibernate.BiomeDAO;
+import uk.ac.ebi.interpro.metagenomics.memi.files.MemiFileWriter;
 import uk.ac.ebi.interpro.metagenomics.memi.model.apro.Submitter;
 import uk.ac.ebi.interpro.metagenomics.memi.model.hibernate.Study;
+import uk.ac.ebi.interpro.metagenomics.memi.services.MemiDownloadService;
 import uk.ac.ebi.interpro.metagenomics.memi.springmvc.model.ViewModel;
 import uk.ac.ebi.interpro.metagenomics.memi.springmvc.model.study.OverviewModel;
 import uk.ac.ebi.interpro.metagenomics.memi.springmvc.model.study.StudyViewModel;
@@ -27,8 +31,10 @@ import uk.ac.ebi.interpro.metagenomics.memi.springmvc.modelbuilder.ViewModelBuil
 import uk.ac.ebi.interpro.metagenomics.memi.springmvc.modelbuilder.study.OverviewModelBuilder;
 
 import javax.annotation.Resource;
-import java.util.LinkedList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.*;
 
 
 /**
@@ -41,12 +47,24 @@ import java.util.List;
 public class OverviewStudyViewController extends AbstractStudyViewController {
 
     private static final Log log = LogFactory.getLog(OverviewStudyViewController.class);
+    private final String VELOCITY_TEMPLATE_LOCATION_PATH = "exportStudy.vm";
+
+    /* The maximum allowed number of characters per column within the study list table*/
+    private final int MAX_CHARS_PER_COLUMN = 35;
+
 
     @Resource
     private RunDAO runDAO;
 
     @Resource
     private BiomeDAO biomeDAO;
+
+    @Resource
+    private VelocityEngine velocityEngine;
+
+    @Resource
+    private MemiDownloadService downloadService;
+
 
     protected String getModelViewName() {
         return "tabs/study/overview";
@@ -81,6 +99,49 @@ public class OverviewStudyViewController extends AbstractStudyViewController {
     public
     @ResponseBody
     String ajaxLoadRunData(@PathVariable final String studyId) throws JsonIOException {
+        List<QueryRunsForProjectResult> runs = getProjectRuns(studyId);
+
+        Gson gson = new Gson();
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.add("data", gson.toJsonTree(runs));
+//        System.out.println("Size of the json: " + jsonResponse.toString());
+        return jsonResponse.toString();
+    }
+
+    @RequestMapping(value = MGPortalURLCollection.PROJECT_OVERVIEW + "/doExport")
+    public ModelAndView doExportStudy(@PathVariable final String studyId,
+                                      HttpServletRequest request,
+                                      HttpServletResponse response,
+                                      final ModelMap model) {
+        log.info("doExportStudy");
+        List<QueryRunsForProjectResult> runs = getProjectRuns(studyId);
+
+        if (runs != null && runs.size() > 0) {
+            List<String> columnNames = new ArrayList<>();
+            columnNames.add("Sample Name");
+            columnNames.add("Sample ID");
+            columnNames.add("Run ID");
+            columnNames.add("Experiment type");
+            columnNames.add("Release version");
+
+            //Create velocity spring_model
+            Map<String, Object> velocityModel = new HashMap<String, Object>();
+            velocityModel.put("tableHeaderNames", columnNames);
+            velocityModel.put("runs", runs);
+            velocityModel.put("columnLength", MAX_CHARS_PER_COLUMN);
+            //Create file content
+            String fileContent = VelocityTemplateWriter.createFileContent(velocityEngine, VELOCITY_TEMPLATE_LOCATION_PATH, velocityModel);
+            File file = MemiFileWriter.writeCSVFile(fileContent);
+            String fileName = MemiTools.createFileName("mg_project_summary_");
+            if (file != null && file.canRead()) {
+                downloadService.openDownloadDialog(response, request, file, fileName, true);
+            }
+        }
+
+        return null;
+    }
+
+    protected List<QueryRunsForProjectResult> getProjectRuns(String studyId) {
         List<QueryRunsForProjectResult> runs = new LinkedList<QueryRunsForProjectResult>();
         Study study = getSecuredEntity(studyId);
         if (study != null) {
@@ -98,12 +159,7 @@ public class OverviewStudyViewController extends AbstractStudyViewController {
                 }
             }
         }
-
-        Gson gson = new Gson();
-        JsonObject jsonResponse = new JsonObject();
-        jsonResponse.add("data", gson.toJsonTree(runs));
-//        System.out.println("Size of the json: " + jsonResponse.toString());
-        return jsonResponse.toString();
+        return runs;
     }
 
     /**
