150338d069316d3a6ebd711e1ea3d9bdff54ec06
maxim <ente12>
2017-09-26 11:52:10 +0100

    Merged in DB schema migration model changes.

--- memi/memi-model/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/model/hibernate/PipelineReleaseTool.java
+++ memi/memi-model/src/main/java/uk/ac/ebi/interpro/metagenomics/memi/model/hibernate/PipelineReleaseTool.java
@@ -28,7 +28,7 @@ public class PipelineReleaseTool implements Serializable {
     private BigDecimal toolGroupId;
 
     // An extra column on the pipeline release/tools join table
-    @Column(name = "HOW_TOOL_USED_DESC", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "HOW_TOOL_USED_DESC", nullable = false, columnDefinition = "LONGTEXT")
     private String howToolUsedDesc;
 
     public PipelineReleaseTool() {
